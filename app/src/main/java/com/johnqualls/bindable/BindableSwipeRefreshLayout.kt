c7845d5661794796d1423b72fa5f3b27a33e6d22
John Qualls <altonqualls@gmail.com>
2019-09-15 11:27:04 -0600

    Remove unused interface

--- app/src/main/java/com/johnqualls/bindable/BindableSwipeRefreshLayout.kt
+++ app/src/main/java/com/johnqualls/bindable/BindableSwipeRefreshLayout.kt
@@ -3,10 +3,6 @@ package com.johnqualls.bindable
 import androidx.databinding.BindingAdapter
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 
-interface BindableSwipeRefreshLayout {
-    fun isRefreshing(isRefreshing: Boolean)
-}
-
 @BindingAdapter("isRefreshing")
 fun bindIsRefreshing(swipeRefreshLayout: SwipeRefreshLayout, isRefreshing: Boolean) {
     swipeRefreshLayout.isRefreshing = isRefreshing
