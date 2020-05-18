52f1df78a913b1dc271d1da04cf5b03ab4c60ff5
John Qualls <altonqualls@gmail.com>
2020-05-18 16:44:41 -0600

    Add threading contract KDoc and reworded parameter documentation

--- udf/src/main/java/com/johnqualls/udf/BaseViewModel.kt
+++ udf/src/main/java/com/johnqualls/udf/BaseViewModel.kt
@@ -7,11 +7,13 @@ import androidx.lifecycle.ViewModel
 /**
  * This class provides the boilerplate code needed for a UDF implementation in the ViewModel layer.
  *
- * @param T The type used to encapsulate incoming view events in [processInput]
- * @param U The type used to encapsulate the view state to later be rendered by the UI
+ * This class must only be accessed from the main UI thread.
+ *
+ * @param T The type used for incoming view events in [processInput]
+ * @param U The type used for the view state to later be rendered by the UI
  * @param V The type used to represent view effects, which are one time view events that are not
  * persisted
- * @param initialViewState The initial view state
+ * @param initialViewState The initial view state [U]
  */
 abstract class BaseViewModel<T, U : Any, V>(initialViewState: U) : ViewModel() {
     private val mutableViewState = MutableLiveData(initialViewState)
